a717a8b2a8cb42879152f64a9dda4ea50265912c
Nicolas Ming <nicming@nicolasming.com>
2022-07-25 15:01:39 -0700

    made a function
    woo

--- Value Cubed/Value Cubed.cpp
+++ Value Cubed/Value Cubed.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 double number;
 
+int cubeIt(int number);
+
 int main()
 {
     cout << "Give me a number and I will cube it for ya, then hit enter: ";
     cin >> number;
-    cout << "Here is " << number << " CUBED: " << (number * number * number);
+
+    cout << "Here is " << number << " CUBED: " << cubeIt(number);
 }
 
+int cubeIt(int number) {
+    return number * number * number;
+}
